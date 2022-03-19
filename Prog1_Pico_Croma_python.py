ddac0b0146db25f8f771987fbda40359dbc0ae75
gbrotto <72582347+gbrotto@users.noreply.github.com>
2022-03-19 14:01:39 +0100

    chromatographic peaks in python
    Prog1_Pico_Croma_python - Criando um picos cromatográficos in python "Creating a chromatographic peaks in python"
    Prog2_Pico_Croma_python - Adicionando assimetria em um pico cromatografico em python "Adding asymmetry to a chromatographic peak in python"

---
+++ Prog1_Pico_Croma_python.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 18 16:53:11 2022
+
+@author: gbrotto
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def gauss(t, t_max, width):
+    f = np.exp(-np.log(2)*((t-t_max)/(width/2))**2)
+    return f
+
+N = 500
+t = np.linspace(0, 10, N)
+t_max = 7.5
+width = .25
+height = 10
+
+dp = .02
+
+erro = dp*np.random.randn(N)
+
+f = height*gauss(t, t_max, width)+erro
+
+plt.plot(t, f, 'b-')
+
+plt.xlabel('Time, min')
+plt.ylabel('Absorbance, mAu')    
---
+++ Prog2_Pico_Croma_python.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 19 11:06:35 2022
+
+@author: gbrotto
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def gauss(t, t_max, width):
+    f = np.exp(-np.log(2)*((t-t_max)/(width/2))**2)
+    return f
+
+def gauss_sk(t, t_max, width, tail):
+    f = np.zeros(np.size(t))
+    ft0 = (1+2*tail*(t-t_max)/width) > 0
+    f[ft0] = np.exp(-np.log(2)*((np.log(1+2*tail*(t[ft0]-t_max)/width)/tail)**2))
+    return f
+
+N = 500
+t = np.linspace(0, 10, N)
+t_max = 7.5
+width = .25
+height = 10
+tail = .4
+
+dp = .02
+erro = dp*np.random.randn(N)
+#f = height*gauss(t, t_max, width)+erro
+
+f = height*gauss_sk(t, t_max, width, tail)+erro
+
+plt.plot(t, f, 'b-')
+
+plt.xlabel('Time, min')
+plt.ylabel('Absorbance, mAu')    
